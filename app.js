29ab10060515f4e8e289722c61262cb7171597ce
Selçuk Çalışkan <45595456+SelcukCaliskan@users.noreply.github.com>
2020-08-01 11:44:40 +0300

    DINAMIC REMOVE METHOD
    Signed-off-by: Selçuk Çalışkan <45595456+SelcukCaliskan@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,18 @@
+//Dinamik Element Silme
+
+const todoList =document.querySelector("ul.list-group");
+const todos=document.querySelectorAll("li.list-group-item");
+
+
+//Remove Metodu
+//todos[1].remove();
+
+
+//Remove Child
+//Son cocuğu silmek için yaptık başka çocuklarda bu şekilde silinebilir.
+//todoList.removeChild(todoList.lastElementChild);
+
+//4.cocuğu sildik bu şekilde..
+//todoList.removeChild(todos[3]);
+console.log(todos);
+console.log(todoList);
